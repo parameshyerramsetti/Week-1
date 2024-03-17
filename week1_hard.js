e9f145bcd3001824968e881b2e5347b928991873
parameshyerramsetti <162950137+parameshyerramsetti@users.noreply.github.com>
2024-03-17 15:34:59 +0530

    Add files via upload

---
+++ week1_hard.js
@@ -0,0 +1,13 @@
+function calculate(c)
+{
+    return f=(9/5)*c+32
+
+}
+function calculate1(f)
+{
+    return c=(5/9)*(f-32)
+}
+let result=calculate(10)
+let result1=calculate1(10)
+console.log(result)
+console.log(result1)
---
+++ week1_medium.js
@@ -0,0 +1,8 @@
+const pi=3.14
+function calculate(r)
+{
+    return pi*r*r
+}
+let result=calculate(3)
+console.log(result)
+
